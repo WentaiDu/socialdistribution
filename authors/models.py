d82a8777c96c27c72f947368d195091b2791c176
solitariaa <66712118+solitariaa@users.noreply.github.com>
2021-10-11 04:40:50 -0600

    change models

--- authors/models.py
+++ authors/models.py
@@ -8,20 +8,12 @@ class Type(models.Model):
     TYPE_CHOICES=["author","Follow","post","comment","Like","liked","inbox"]
     type = models.CharField(choices =TYPE_CHOICES)
 '''
+class Inbox(models.Model):
+    inbox_type = models.CharField(max_length=100, default="", blank=False,verbose_name="type")
 
-class Author(models.Model):
-    author_type = models.CharField(max_length=30,default="", blank=False,verbose_name="type")
-    author_id = models.UUIDField(primary_key = True , auto_created = True , default = uuid.uuid4, editable = False )
-    host = models.CharField(max_length=20,default='127.0.0.1:5454',editable = False)
-    displayName = models.CharField(max_length=30, default="", blank=False)
-    github = models.SlugField(unique=True, blank=False)
-
-
-
-
+    
 class Comment(models.Model):
     comment_type = models.CharField(max_length=100, default="", blank=False,verbose_name="type")
-    comment_author = models.ForeignKey(Author,on_delete=models.CASCADE,verbose_name="author")
     comment = models.TextField(default="", blank=False)
     contentType = models.ForeignKey(ContentType,on_delete=models.CASCADE)
     published = models.DateTimeField(auto_now_add=True)
@@ -34,7 +26,6 @@ class Category(models.Model):
 
     def __str__(self):
        return self.category
-
 #Confused about which part should be in post and which part should be shown by pagination.
 class Post(models.Model):
     post_type = models.CharField(max_length=100, default="", blank=False,verbose_name="type")
@@ -45,8 +36,26 @@ class Post(models.Model):
     description = models.TextField(default="")
     contentType = models.ForeignKey(ContentType,on_delete=models.CASCADE,)
     content = models.FileField()
-    post_author = models.ForeignKey(Author,on_delete=models.CASCADE,verbose_name="author")
     category = models.ManyToManyField(Category)
+    items = models.ForeignKey(Inbox,on_delete=models.CASCADE)
+
+
+class Author(models.Model):
+    author_type = models.CharField(max_length=30,default="", blank=False,verbose_name="type")
+    author_id = models.UUIDField(primary_key = True , auto_created = True , default = uuid.uuid4, editable = False )
+    host = models.CharField(max_length=20,default='127.0.0.1:5454',editable = False)
+    displayName = models.CharField(max_length=30, default="", blank=False)
+    github = models.SlugField(unique=True, blank=False)
+    inbox = models.ForeignKey(Inbox,on_delete=models.CASCADE,verbose_name="author")
+    comment = models.ForeignKey(Comment,on_delete=models.CASCADE,verbose_name="author")
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,verbose_name="author")
+
+
+
+
+
+
+
 
 
 class Like(models.Model):
@@ -56,7 +65,4 @@ class Like(models.Model):
     like_author = models.ForeignKey(Author,on_delete=models.CASCADE,verbose_name="author")
     object = models.ForeignKey(Post,on_delete=models.CASCADE)
 
-class Inbox(models.Model):
-    inbox_type = models.CharField(max_length=100, default="", blank=False,verbose_name="type")
-    inbox_author = models.ForeignKey(Author,on_delete=models.CASCADE,verbose_name="author")
-    items = models.ForeignKey(Post,on_delete=models.CASCADE)
+
