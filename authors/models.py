e7d5d4a74954a125ac85534dbbaf72e7a8e39518
solitariaa <66712118+solitariaa@users.noreply.github.com>
2021-10-16 03:45:46 -0600

    1‘ ’ '

--- authors/models.py
+++ authors/models.py
@@ -1,32 +1,22 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 import uuid
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import fields
-from django.http import HttpRequest
-'''
-class Type(models.Model):
-    TYPE_CHOICES=["author","Follow","post","comment","Like","liked","inbox"]
-    type = models.CharField(choices =TYPE_CHOICES)
-'''
+
+
+class Author(models.Model):
+    author_type = models.CharField(max_length=30,default="", blank=False)
+    author_id = models.UUIDField(primary_key = True , auto_created = True , default = uuid.uuid4, editable = False)
+    host = models.GenericIPAddressField(max_length=20,default='127.0.0.1:5454',editable = False)
+    displayName = models.CharField(max_length=30, default="", blank=False)
+    url = models.URLField()
+    github = models.SlugField(unique=True, blank=False)
+    profileImage = models.URLField()
+
 class Inbox(models.Model):
-    inbox_type = models.CharField(max_length=100, default="", blank=False,verbose_name="type")
+    inbox_type = models.CharField(max_length=100, default="", blank=False)
+    inbox_author = models.OneToOneField(Author,on_delete=models.CASCADE,default='')
 
-
-class Comment(models.Model):
-    comment_type = models.CharField(max_length=100, default="", blank=False,verbose_name="type")
-    comment = models.TextField(default="", blank=False)
-    contentType = models.ForeignKey(ContentType,on_delete=models.CASCADE)
-    published = models.DateTimeField(auto_now_add=True)
-    comment_id = models.UUIDField(primary_key = True , auto_created = True , default = uuid.uuid4, editable = False,verbose_name="id")
-    comment_object= fields.GenericForeignKey('contentType','comment_id')
-
-
-class Category(models.Model):
-    category = models.CharField(max_length=100)
-
-    def __str__(self):
-       return self.category
-#Confused about which part should be in post and which part should be shown by pagination.
 class Post(models.Model):
     post_type = models.CharField(max_length=100, default="", blank=False,verbose_name="type")
     title = models.CharField(max_length=100, default="", blank=False)
@@ -34,28 +24,29 @@ class Post(models.Model):
     source = models.URLField(default="")
     origin = models.URLField(default="")
     description = models.TextField(default="")
-    contentType = models.ForeignKey(ContentType,on_delete=models.CASCADE,)
+    contentType = models.ForeignKey(ContentType,on_delete=models.CASCADE)
     content = models.FileField()
-    category = models.ManyToManyField(Category)
-    items = models.ForeignKey(Inbox,on_delete=models.CASCADE,default='')
+    post_author = models.ForeignKey(Author,on_delete=models.CASCADE,default='')
+    categories = ArrayField(models.CharField(max_length=200), blank=True)
+    count = models.IntegerField()
+    comments = models.URLField()
+    published = models.DateTimeField(auto_now_add=True)#USE_TZ=True in settings.py
+    visibility = ArrayField(models.CharField(max_length=200), blank=True)
+    unlisted = models.BooleanField(default=False, null=False)
 
-
-class Author(models.Model):
-    author_type = models.CharField(max_length=30,default="", blank=False,verbose_name="type")
-    author_id = models.UUIDField(primary_key = True , auto_created = True , default = uuid.uuid4, editable = False )
-    host = models.CharField(max_length=20,default='127.0.0.1:5454',editable = False)
-    displayName = models.CharField(max_length=30, default="", blank=False)
-    github = models.SlugField(unique=True, blank=False)
-    inbox = models.ForeignKey(Inbox,on_delete=models.CASCADE,verbose_name="author",default='')
-    comment = models.ForeignKey(Comment,on_delete=models.CASCADE,verbose_name="author",default='')
-    post = models.ForeignKey(Post,on_delete=models.CASCADE,verbose_name="author",default='')
-
+class Comment(models.Model):
+    comment_type = models.CharField(max_length=100, default="", blank=False,verbose_name="type")
+    comment_author = models.ForeignKey(Author,on_delete=models.CASCADE,default='')
+    comment = models.TextField(default="", blank=False)
+    contentType = models.ForeignKey(ContentType,on_delete=models.CASCADE)
+    published = models.DateTimeField(auto_now_add=True)
+    comment_id = models.UUIDField(primary_key = True , auto_created = True , default = uuid.uuid4, editable = False,verbose_name="id") 
+    comment_post = models.ForeignKey(Post,on_delete=models.CASCADE,default='')
 
 class Like(models.Model):
     content = models.URLField(default="", blank=False,verbose_name="@context")
     summary = models.CharField(max_length=100, default="", blank=False)
     type = models.CharField(max_length=100, default="", blank=False)
-    like_author = models.ForeignKey(Author,on_delete=models.CASCADE,verbose_name="author")
-    object = models.ForeignKey(Post,on_delete=models.CASCADE)
-
+    like_author = models.OneToOneField(Author,on_delete=models.CASCADE,verbose_name="author")
+    object = models.URLField()
 
