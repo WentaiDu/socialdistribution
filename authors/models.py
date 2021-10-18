fceef9eaf0849b2a554dccd14c4fd839d3c4baf5
Rain981012 <siyuan9@ualberta.ca>
2021-10-18 12:06:09 -0600

    basic template

--- authors/models.py
+++ authors/models.py
@@ -10,8 +10,12 @@ class Author(models.Model):
     host = models.GenericIPAddressField(max_length=20,default='127.0.0.1:5454',editable = False)
     displayName = models.CharField(max_length=30, default="", blank=False)
     url = models.URLField()
-    github = models.SlugField(unique=True, blank=False)
+    #github = models.SlugField(unique=True, blank=False)
+    github = models.CharField(null = True,blank=False, max_length=50)
     profileImage = models.URLField()
+    def __str__(self):
+        return self.displayName+'  ' +str(self.author_id)
+
 
 class Post(models.Model):
     post_type = models.CharField(max_length=100, default="", blank=False,verbose_name="type")
@@ -22,20 +26,32 @@ class Post(models.Model):
     description = models.TextField(default="")
     contentType = models.ForeignKey(ContentType,on_delete=models.CASCADE)
     content = models.FileField()
-    post_author = models.ForeignKey(Author,on_delete=models.CASCADE,default='')
-    categories = ArrayField(models.CharField(max_length=200), blank=True)
+    #post_author = models.ForeignKey(Author,on_delete=models.CASCADE,default='')
+    #categories = ArrayField(models.CharField(max_length=200), blank=True)
     count = models.IntegerField()
     comments = models.URLField()
     published = models.DateTimeField(auto_now_add=True)#USE_TZ=True in settings.py
-    visibility = ArrayField(models.CharField(max_length=200), blank=True)
+    #visibility = ArrayField(models.CharField(max_length=200), blank=True)
     unlisted = models.BooleanField(default=False, null=False)
     
 class Inbox(models.Model):
-    inbox_type = models.CharField(max_length=100, default="", blank=False)
-    inbox_author = models.OneToOneField(Author,on_delete=models.CASCADE,default='')
-    item = models.ForeignKey(Post,on_delete=models.CASCADE,default='')
+    inbox_types = models.CharField(max_length=100, default="", blank=True, null=True, verbose_name='type')
+    inbox_titles = models.CharField(max_length=500, default='', blank=True, null=True, verbose_name='title')
+    inbox_ids = models.URLField(default='',primary_key=True)
+    inbox_sources = models.URLField(default='')
+    inbox_origins = models.URLField(default='')
+    inbox_descriptions = models.CharField(max_length=500, default='', blank=True, null=True, verbose_name='description')
+    inbox_content_types = models.CharField(max_length=500, default='', blank=True, null=True,verbose_name='contentType')
+    inbox_contents = models.CharField(max_length=500, default='', blank=True, null=True, verbose_name='content')
+    inbox_authors = models.ForeignKey(Author, on_delete=models.CASCADE,related_name='authors',null=True)
+    #inbox_categories = ArrayField(models.CharField(max_length=200), blank=True, verbose_name='categories')
+    inbox_comments = models.URLField()
+    inbox_published = models.DateTimeField(auto_now_add=True)
+    inbox_visibility = models.CharField(max_length=100, default="", blank=True, null=True, verbose_name='visibility')
+    inbox_unlisted = models.BooleanField(default=False, null=False)
 
 
+    
 class Comment(models.Model):
     comment_type = models.CharField(max_length=100, default="", blank=False,verbose_name="type")
     comment_author = models.ForeignKey(Author,on_delete=models.CASCADE,default='')
