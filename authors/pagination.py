84c88767e8afcdd7aa3ebdab1bd35a1461b96bb5
solitariaa <ziying7@ualberta.ca>
2021-10-18 13:25:45 -0600

    1

--- authors/pagination.py
+++ authors/pagination.py
@@ -3,13 +3,24 @@ from rest_framework.response import Response
 from collections import OrderedDict
 
 
-class MyCustomPagination(PageNumberPagination):
+class AuthorPagination(PageNumberPagination):
 
     page_size_query_param = "size"
-
     page_query_param = "page"
     def get_paginated_response(self, data):
         return Response(OrderedDict([
             ('type', "authors"),
+            ('items', data)
+         ]))
+
+class CommentPagination(PageNumberPagination):
+
+    page_size_query_param = "size"
+    page_query_param = "page"
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('type', "comments"),
+            ('page',self.page.paginator.number),
+            ('size',self.page.paginator.per_page),
             ('items', data)
          ]))
