4cf9b6676bdb41bc8002e5fdede002768a019ff0
Amos-lii <78457001+Amos-lii@users.noreply.github.com>
2021-10-27 01:05:43 -0600

    Update models.py

--- authors/models.py
+++ authors/models.py
@@ -22,23 +22,41 @@ class Inbox(models.Model):
 # #     item = models.ManyToManyField(Post,on_delete=models.CASCADE,default='')
 
 class Post(models.Model):
-    items = models.ForeignKey(Inbox, related_name='items', on_delete=models.CASCADE)
-    post_type = models.CharField(max_length=100, default="", blank=False,verbose_name="type")
+    class Visibility(models.TextChoices):
+        PUBLIC='PUBLIC'
+        PRIVATE='PRIVATE'
+        FRIENDONLY='FRIENDS'
+
+    class ContentType(models.TextChoices):
+        MARKDOWN = 'text/markdown'
+        PLAIN = 'text/plain'
+        APPLICATION = 'application/base64'
+        IMAGE_PNG = 'image/png;base64'
+        IMAGE_JPEG = 'image/jpeg;base64'
+
+
+
+
+    # items = models.ForeignKey(Inbox, related_name='items', on_delete=models.CASCADE)
+    type = models.CharField(max_length=100, default="", blank=False,verbose_name="type")
     title = models.CharField(max_length=100, default="", blank=False)
     post_id = models.UUIDField(primary_key = True, auto_created = True , default = uuid.uuid4, editable = False,verbose_name="id")
     source = models.URLField(default="")
     origin = models.URLField(default="")
     description = models.TextField(default="")
-    contentType = models.ForeignKey(ContentType,on_delete=models.CASCADE)
-    image_content = models.FileField()
-    text_content = models.CharField(max_length=500, default='',blank=True, null=True)
-    post_author = models.ForeignKey(Author,on_delete=models.CASCADE,default='')
+    contentType = models.CharField(max_length=20, choices=ContentType.choices, default=ContentType.PLAIN)
+    # image_content = models.FileField()
+    # text_content = models.CharField(max_length=500, default='',blank=True, null=True)
+    author = models.ForeignKey(Author,related_name='author',on_delete=models.CASCADE,default='')
+    content=models.TextField()
     #categories = ArrayField(models.CharField(max_length=200), blank=True)
     #count = models.IntegerField()
+    #scrcomment
     comments = models.URLField()
     published = models.DateTimeField(auto_now_add=True)#USE_TZ=True in settings.py
-    #visibility = ArrayField(models.CharField(max_length=200), blank=True)
+    visibility = models.CharField(max_length=20,choices=Visibility.choices , default=Visibility.PUBLIC)
     unlisted = models.BooleanField(default=False, null=False)
+
 
 class Comment(models.Model):
     comment_type = models.CharField(max_length=100, default="", blank=False,verbose_name="type")
