c9ceb44da6422259f26618c6df86293518184e21
Amos-lii <78457001+Amos-lii@users.noreply.github.com>
2021-10-27 01:03:23 -0600

    Update urls.py

--- authors/urls.py
+++ authors/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import include
 from authors import views
 from rest_framework.authtoken import views as drf_auth_views
 urlpatterns = [
+    re_path(r'author/(?P<author_id>[(-z)]{36})/posts/(?P<post_id>[(-z)]{36})/', views.PostDetail.as_view()),
+    re_path(r'author/(?P<author_id>[(-z)]{36})/posts/', views.PostList.as_view()),
     re_path(r'author/(?P<author_id>[(-z)]{36})/posts/(?P<post_id>[(-z)]{36})/comments', views.CommentList.as_view()),
     re_path(r'author/(?P<author_id>[(-z)]{36})/inbox', views.InboxList.as_view()),
     re_path(r'author/(?P<author_id>[(-z)]{36})/', views.AuthorDetail.as_view()),
