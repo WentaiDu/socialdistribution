4ec430077b8ad4357d86dd263e118c01fcc28365
Amos-lii <78457001+Amos-lii@users.noreply.github.com>
2021-10-27 01:05:00 -0600

    Update pagination.py

--- authors/pagination.py
+++ authors/pagination.py
@@ -23,3 +23,15 @@ class CommentPagination(PageNumberPagination):
             ('size',self.page.paginator.per_page),
             ('items', data)
          ]))
+    
+class PostPagination(PageNumberPagination):
+
+    page_size_query_param = "size"
+    page_query_param = "page"
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('type', "posts"),
+            ('page',self.page.paginator.number),
+            ('size',self.page.paginator.per_page),
+            ('items', data)
+         ]))
