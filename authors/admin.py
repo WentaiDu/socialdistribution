d0831d5a2612677051ba6cd63174e1beb1a8df6b
Amos-lii <78457001+Amos-lii@users.noreply.github.com>
2021-10-27 00:59:46 -0600

    Delete authors directory

--- authors/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . import models
-
-admin.site.register(models.Author)
-admin.site.register(models.Post)
-admin.site.register(models.Comment)
-admin.site.register(models.Like)
