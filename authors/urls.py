3ad704476777bb0e69325db44899a5ffa4301a5e
Rain981012 <siyuan9@ualberta.ca>
2021-11-25 04:19:27 -0700

    finish inbox

--- authors/urls.py
+++ authors/urls.py
@@ -16,8 +16,8 @@ urlpatterns = [
     re_path(r'author/(?P<author_id>[(-z)]{36})/posts/(?P<post_id>[(-z)]{36})/comments/(?P<comment_id>[(-z)]{36})/likes', views.LikesCommentList.as_view()),
     re_path(r'author/(?P<author_id>[(-z)]{36})/posts/(?P<post_id>[(-z)]{36})/comments', views.CommentList.as_view()),
   
+    re_path(r'author/(?P<author_id>[(-z)]{36})/inbox', views.InboxView.as_view()),
 
-    re_path(r'author/(?P<author_id>[(-z)]{36})/inbox', views.InboxList.as_view()),
     re_path(r'author/(?P<author_id1>[(-z)]{36})/followers/(?P<author_id2>[(-z)]{36})', views.FollowerDetailView.as_view()),
     re_path(r'author/(?P<author_id>[(-z)]{36})/followers', views.FollowerList.as_view()),
 
