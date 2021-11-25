590f5b08037c4161408af39e9a7e82c4c3317755
WentaiDu <78457517+WentaiDu@users.noreply.github.com>
2021-11-25 00:22:35 -0700

    fix

--- authors/urls.py
+++ authors/urls.py
@@ -8,16 +8,16 @@ urlpatterns = [
 
     re_path(r'author/(?P<author_id>[(-z)]{36})/posts/(?P<post_id>[(-z)]{36})/likes',views.Likes_list.as_view()),
     re_path(r'author/(?P<author_id>[(-z)]{36})/liked',views.LikedList.as_view()),
-    # re_path(r'author/(?P<author_id>[(-z)]{36})/posts/(?P<post_id>[(-z)]{36})/', views.PostDetail.as_view()),
-    # re_path(r'author/(?P<author_id>[(-z)]{36})/posts/', views.PostList.as_view()),
+    re_path(r'author/(?P<author_id>[(-z)]{36})/posts/(?P<post_id>[(-z)]{36})/', views.PostDetail.as_view()),
+    re_path(r'author/(?P<author_id>[(-z)]{36})/posts/', views.PostList.as_view()),
     re_path(r'author/(?P<author_id>[(-z)]{36})/posts/(?P<post_id>[(-z)]{36})/comments/(?P<comment_id>[(-z)]{36})/likes', views.LikesCommentList.as_view()),
     re_path(r'author/(?P<author_id>[(-z)]{36})/posts/(?P<post_id>[(-z)]{36})/comments', views.CommentList.as_view()),
   
-    # re_path(r'author/(?P<author_id>[(-z)]{36})/inbox', views.InboxList.as_view()),
-    # re_path(r'author/(?P<author_id1>[(-z)]{36})/followers/(?P<author_id2>[(-z)]{36})', views.FollowerDetailView.as_view()),
-    # re_path(r'author/(?P<author_id>[(-z)]{36})/followers', views.FollowerList.as_view()),
+    re_path(r'author/(?P<author_id>[(-z)]{36})/inbox', views.InboxList.as_view()),
+    re_path(r'author/(?P<author_id1>[(-z)]{36})/followers/(?P<author_id2>[(-z)]{36})', views.FollowerDetailView.as_view()),
+    re_path(r'author/(?P<author_id>[(-z)]{36})/followers', views.FollowerList.as_view()),
 
-    # re_path(r'author/(?P<author_id>[(-z)]{36})/', views.AuthorDetail.as_view()),
+    re_path(r'author/(?P<author_id>[(-z)]{36})/', views.AuthorDetail.as_view()),
 
     path('login/',views.LoginAPI.as_view(),name='login'),
     path('author/',views.SignupAPI.as_view(),name='signup'),
