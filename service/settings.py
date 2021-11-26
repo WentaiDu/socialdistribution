e7b1a5ffbeccfb4fa35b8b9ea7ca659cf9848945
Amos-lii <haoyuan8@ualberta.ca>
2021-11-25 18:11:01 -0700

    beckend

--- service/settings.py
+++ service/settings.py
@@ -169,6 +169,6 @@ django_on_heroku.settings(locals()) # bottom of the file
 
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
